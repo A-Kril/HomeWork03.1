9f62c867d8d1114115a7acb5f9fd06923fc60712
A-Kril <a.krilevets@gmail.com>
2017-06-14 19:09:24 +0300

    Fixed

--- src/QueueRun.java
+++ src/QueueRun.java
@@ -1,3 +1,8 @@
+/*Alexandr Krilevets
+ДЗ 03.1
+Необходимо проанализировать коллекции, созданные для ДЗ по модулю 1.2 и выявить потенциально опасные моменты,
+в которых могут произойти сбои. Создать кастомные исключения и бросать в случае ошибки.
+*/
 public class QueueRun {
     public static void main(String[] args) throws Exception{
         QueueTest.queueTest();
